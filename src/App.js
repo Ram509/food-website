269683cc8d049427d538a4f82000ca74bc4ab87c
Ramesh S <98960684+Ram509@users.noreply.github.com>
2024-05-03 08:43:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Menu from "./Components/Menu/menu";
+import Home from "./Components/Home/home";
+import About from "./Components/About/about";
+import Login from "./Components/Login/login";
+import Contact from "./Components/Contact/contact";
 
 const App = () => {
   return (
-    <div>
-      <h1>hello everyone</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
