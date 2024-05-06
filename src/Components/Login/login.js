ecdde5e7dccdc865dd2e0c3df440a1f10d5e4d6c
Ramesh S <98960684+Ram509@users.noreply.github.com>
2024-05-06 21:01:29 +0530

    Update login.js

--- src/Components/Login/login.js
+++ src/Components/Login/login.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import '/LoginForm.css';
 
-const contact = () => {
-  return (
-    <div>Login</div>
-  )
-}
-
-export default login
+const LoginForm = () => {
+   return(
+   <div className='wrapper'>
+    <form action="">
+    <h1>Login</h1>
+   <div className="input-box">
+    <input type="text" placeholder="Username' required/>
+    </div>
+<div className="input-box">
+    <input type="password" placeholder="Password" required/>
+ </div>
+ <div className="remember-forget">
+    <label><input type="checkbox" />Remember me</label>
+    <a href="#">Forget password?</a>
+</div>
+  <button type="submit/>Login</button>
+   <div className='register-link">
+<p>Don't have an account? <a href="#/a>Register</p>
+ </div>
+</form>
+</div>
